8358601cb3ab019ed7ea6856869a67dffa47d0cf
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-12-07 22:46:37 +0100

    closer and closer

--- network.py
+++ network.py
@@ -1,73 +1,91 @@
 import numpy as np
 from reader import Reader
 
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
 
-def sigmoid_deriv(z):
-    return sigmoid(z)*(1-sigmoid(z))
+def d_sigmoid(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
+def relu(z):
+    return np.maximum(0, z)
+
+def d_relu(z):
+    return np.where(z > 0, 1, 0)
 
 class Network:
-    def __init__(self, sizes, activation, a_derivative, batch_size=10):
-        self.activation = activation
-        self.a_derivative = a_derivative
-        self.num_layers = len(sizes)
+    def __init__(self, sizes, activation, d_activation):
         self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-        self.biases_updates = self.biases
-        self.batch_size = batch_size
-        for i in range(len(self.biases_updates)):
-            self.biases_updates[i] = np.zeros_like(self.biases_updates[i])
-        self.weights_updates = self.weights
-        for i in range(len(self.weights_updates)):
-            self.weights_updates[i] = np.zeros_like(self.weights_updates[i])
+        self.num_layers = len(sizes)
+        self.activation = activation
+        self.d_activation = d_activation
 
-    def train_step(self, input, expected_out):
-        anow = np.frombuffer(input, dtype=np.uint8)
-        anow = anow.reshape((784, 1))
-        anow = anow / 255.0
-        a = [anow]
-        ztab = []
+        self.biases = [np.random.randn(size, 1) for size in sizes[1:]]
+        self.weights = [np.random.randn(size2, size1) / np.sqrt(size1) for size1, size2 in zip(sizes[:-1], sizes[1:])]
+
+        self.biases_upd = [np.zeros(b.shape) for b in self.biases]
+        self.weights_upd = [np.zeros(w.shape) for w in self.weights]
+
+        self.a_arr = []
+        self.z_arr = []
+
+    def feedforward(self, a):
+        self.a_arr = [a]
+        self.z_arr = [self.activation(a)]
         for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, anow) + b
-            ztab.append(z)
-            anow = self.activation(z)
-            a.append(anow)
+            z = np.dot(w, a) + b
+            a = self.activation(z)
+            self.z_arr.append(z)
+            self.a_arr.append(a)
+        return a
 
-        dera = (a[-1] - expected_out) * 2
-        self.biases_updates[-1] -= (dera * self.a_derivative(ztab[-1])) / self.batch_size
-        self.weights_updates[-1] -= np.dot(dera * self.a_derivative(ztab[-1]), a[-2].transpose()) / self.batch_size
+    def backprop(self, y):
+        for i in range(self.num_layers - 1, 0, -1):
+            if i == self.num_layers - 1:
+                delta = (self.a_arr[i] - y) * self.d_activation(self.z_arr[i])
+            else:
+                delta = np.dot(self.weights[i].T, delta) * self.d_activation(self.z_arr[i])
+            self.biases_upd[i-1] = delta
+            self.weights_upd[i-1] = np.dot(delta, self.a_arr[i-1].T)
 
-        for l in range(self.num_layers-3, -1, -1):
-            dera = np.dot(self.weights[l+1].transpose(), dera)
-            self.biases_updates[l] -= dera * self.a_derivative(ztab[l])
-            self.weights_updates[l] -= np.dot(dera * self.a_derivative(ztab[l]), a[l].transpose())
+    def update(self, lr, batch_size):
+        self.biases = [b - (lr / batch_size) * b_upd for b, b_upd in zip(self.biases, self.biases_upd)]
+        self.weights = [w - (lr / batch_size) * w_upd for w, w_upd in zip(self.weights, self.weights_upd)]
 
-    def train(self, eta):
-        r = Reader('tests/train-images-idx3-ubyte', 'tests/train-labels-idx1-ubyte')
-        # for i in range(r.set_size // self.batch_size):
-        for i in range(200):
-            print("Epoch {0}".format(i))
-            for _ in range(self.batch_size):
-                test = r.next_test()
-                expected = np.zeros((10, 1))
-                expected[np.frombuffer(test[1], dtype=np.uint8)] = 1.0
-                self.train_step(test[0], expected)
-            self.update(eta, r.set_size)
-        print(self.biases)
+        self.biases_upd = [np.zeros(b.shape) for b in self.biases]
+        self.weights_upd = [np.zeros(w.shape) for w in self.weights]
 
-    def update(self, eta, batch_size):
-        for i in range(len(self.biases)):
-            self.biases[i] -= eta * self.biases_updates[i]
-        for i in range(len(self.weights)):
-            self.weights[i] -= eta * self.weights_updates[i]
 
-        for i in range(len(self.biases_updates)):
-            self.biases_updates[i] = np.zeros_like(self.biases_updates[i])
+    def onehot(self, y):
+        onehot = np.zeros((self.sizes[-1], 1))
+        onehot[y] = 1.0
+        return onehot
 
-        for i in range(len(self.weights_updates)):
-            self.weights_updates[i] = np.zeros_like(self.weights_updates[i])
+    def train(self, train_data, epochs, batch_size, lr):
+        n = len(train_data)
+        for i in range(epochs):
+            correct = 0
+            np.random.shuffle(train_data)
+            batches = [train_data[k:k+batch_size] for k in range(0, n, batch_size)]
+            itr = 0
+            for batch in batches:
+                itr += 1
+                mini_correct = 0
+                for x, y in batch:
+                    if(np.argmax(self.feedforward(x)) == y):
+                        correct += 1
+                        mini_correct += 1
+                    self.backprop(self.onehot(y))
+                self.update(lr, batch_size)
+                if(itr % 100 == 0):
+                    print(f"Mini batch: {mini_correct}/{len(batch)}")
 
-n = Network([784, 16, 16, 10], sigmoid, sigmoid_deriv)
-n.train(0.1)
+            print(f"Epoch {i}:  {correct}/{len(train_data)}")
+
+
+n = Network([784, 16, 16, 10], relu, d_relu)
+r = Reader('tests/train-images-idx3-ubyte', 'tests/train-labels-idx1-ubyte')
+
+t = r.all_tests()
+print(len(t))
+n.train(t, 4, 64, 0.1)
--- reader.py
+++ reader.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Reader:
     def __init__(self, file_images, file_labels):
         self.file_images = open(file_images, 'rb')
@@ -14,9 +16,29 @@ class Reader:
         self.rows = int.from_bytes(self.file_images.read(4), 'big')
         self.cols = int.from_bytes(self.file_images.read(4), 'big')
 
+    def all_tests(self):
+        res = []
+        for _ in range(self.set_size):
+            self.file_images.seek(16 + self.index * self.rows * self.cols)
+            self.file_labels.seek(8 + self.index)
+            self.index += 1
+            pixels = np.frombuffer(self.file_images.read(self.rows * self.cols), dtype=np.uint8)
+            pixels = pixels.reshape((784, 1))
+            pixels = pixels / 255.0
+            label = int.from_bytes(self.file_labels.read(1), 'big')
+            res.append((pixels, label))
 
-    def next_test(self):
-        self.file_images.seek(16 + self.index * self.rows * self.cols)
-        self.file_labels.seek(8 + self.index)
-        self.index += 1
-        return self.file_images.read(self.rows * self.cols), self.file_labels.read(1)
+            pixels = pixels.reshape((28, 28))
+
+            pixels = np.roll(pixels, 1, axis=1)
+            res.append((pixels.reshape((784, 1)), label))
+
+            pixels = np.roll(pixels, -2, axis=1)
+            res.append((pixels.reshape((784, 1)), label))
+
+            pixels = np.roll(pixels, 1, axis=0)
+            res.append((pixels.reshape((784, 1)), label))
+
+            pixels = np.roll(pixels, -2, axis=0)
+            res.append((pixels.reshape((784, 1)), label))
+        return res
